6d4da9d5b1bce93a17678ecfc091982ec36ccb0d
b-b90 <brkicbojan@yahoo.com>
2018-04-12 17:55:10 +0200

    forms

--- src/view/AddingForm.java
+++ src/view/AddingForm.java
@@ -1,5 +1,45 @@
 package view;
 
+import javax.swing.*;
+
 public class AddingForm {
+	
+	JFrame frame = new JFrame("Add new contact");
+	JPanel panel = new JPanel();
+	JLabel nameLabel = new JLabel("Name:");
+	JLabel surnameLabel = new JLabel("Surname:");
+	JLabel numberLabel = new JLabel("Telephone number:");
+	JTextField nameText = new JTextField();
+	JTextField surnameText = new JTextField();
+	JTextField numberText = new JTextField();
+	JButton saveButton = new JButton("Save contact");
+	
+	public void add() {
+		
+		nameLabel.setBounds(15, 40, 120, 20);
+		surnameLabel.setBounds(15, 80, 120, 20);
+		numberLabel.setBounds(15, 120, 120, 20);
+		
+		nameText.setBounds(145, 40, 120, 25);
+		surnameText.setBounds(145, 80, 120, 25);
+		numberText.setBounds(145, 120, 120, 25);
+		
+		saveButton.setBounds(70, 170, 150, 30);
+		
+		panel.add(nameLabel);
+		panel.add(nameText);
+		panel.add(numberLabel);
+		panel.add(numberText);
+		panel.add(surnameLabel);
+		panel.add(surnameText);
+		panel.add(saveButton);
+		panel.setLayout(null);
+		
+		frame.add(panel);
+		frame.setSize(300, 300);
+		frame.setLocation(500, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 
 }
--- src/view/ViewForm.java
+++ src/view/ViewForm.java
@@ -1,5 +1,63 @@
 package view;
 
-public class ViewForm {
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ViewForm implements ActionListener {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel nameLabel1 = new JLabel("Name:");
+	JLabel nameLabel2 = new JLabel();
+	JLabel surnameLabel1 = new JLabel("Surname:");
+	JLabel surnameLabel2 = new JLabel();
+	JLabel numberLabel1 = new JLabel("Telephone number:");
+	JLabel numberLabel2 = new JLabel();
+	JButton closeButton = new JButton("Close");
+	
+	public void view() {
+		
+		nameLabel1.setBounds(20, 20, 120, 20);
+		surnameLabel1.setBounds(20, 60, 120, 20);
+		numberLabel1.setBounds(20, 100, 120, 20);
+		
+		nameLabel2.setBounds(145, 20, 120, 25);
+		surnameLabel2.setBounds(145, 60, 120, 25);
+		numberLabel2.setBounds(145, 100, 120, 25);
+		
+		closeButton.setBounds(90, 180, 150, 30);
+		closeButton.addActionListener(this);
+		
+		panel.setLayout(null);
+		panel.setBackground(Color.lightGray);
+		panel.add(nameLabel1);
+		panel.add(nameLabel2);
+		panel.add(numberLabel1);
+		panel.add(numberLabel2);
+		panel.add(surnameLabel1);
+		panel.add(surnameLabel2);
+		panel.add(closeButton);
+		
+		frame.add(panel);
+		frame.setSize(350, 300);
+		frame.setLocation(500, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		System.exit(0);		
+	}
+
+	public static void main(String[] args) {
+		
+		ViewForm form = new ViewForm();
+		form.view();
+	}
 
 }
